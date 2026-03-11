cd674304529765d2ad2348922b7aac0c0527b272
Jace <jaceh3898@gmail.com>
2026-03-11 12:47:46 -0400

    Halfway done

--- projects/game/game.js
+++ projects/game/game.js
@@ -12,11 +12,14 @@ function SkyHub() {
     clear();
     print("\nWelcome to the SkyCraft Hub!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tBlackSmith");
+        "\n\tBlackSmith," + "\n\tMuseum");
     
     function processInput(input){
         if (input.toLowerCase() === "blacksmith") {
             BlackSmith();
+	} else if {
+		(input.toLowerCase() === "museum") {
+		Museum();
         } else {
             stayHere();
             waitThenCall(SkyHub);
@@ -25,23 +28,41 @@ function SkyHub() {
     waitForInput(processInput);
 }
 
-function locationB() {
+function BlackSmith() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in the BlackSmith Shop!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tSkyHub," + "\n\tMines");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
+        if (input.toLowerCase() === "skyhub") {
+            SkyHub();
+	} else if {
+		(input.toLowerCase() === "mines") {
+		Mines();
         } else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(BlackSmith);
         }
     }
     waitForInput(processInput);
 }
 
+function Museum() {
+	clear();
+	print("\nWelcome to the Museum I advise you to not be in here unless you have the Zombie Head or else I will kill you!");
+	print("\nGo back to the Hub or Else..." + 
+	"\n\tSkyHub");
+
+	function processInput(input){
+	if (input.toLowerCase() === "skyhub") {
+		SkyHub();
+	} else {
+		stayHere();
+		waitThenCall(killUser);
+	}
+		waitForInput(processInput);
+	}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
@@ -49,7 +70,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            SkyHub();
     }
     waitForInput(processInput);
 }
