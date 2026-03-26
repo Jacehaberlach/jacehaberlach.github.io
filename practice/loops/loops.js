80fdd1d91c802bbcab3ec07346c29f4bac7dee34
Jace <jaceh3898@gmail.com>
2026-03-26 12:46:36 -0400

    done

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -2,18 +2,46 @@ let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
 //fill in code for canvas1 here
+for(let y = 0; y <= 200; y += 100){
+	for(let x = 25; x < 800; x += 100){
+		ctx.fillRect(x, y, 50, 20);
+	}
+}
 
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");
 
 //fill in code for canvas2 here
+for(let y = 100; y <= 800; y += 100){
+	for(let x = 25; x < 800; x += 100){
+		if(y > x){
+		ctx.fillRect(x, y, 50, 20);
+		}
+	}
+}
 
 canvas = document.getElementById("canvas3");
 ctx = canvas.getContext("2d");
 
 //fill in code for canvas3 here
 
+for(let y = 0; y < 800; y += 100){
+	for(let x = 0; x < 800; x += 100){
+		if(y != x){
+		ctx.fillRect(x, y, 50, 20);
+		}
+	}
+}
+
 canvas = document.getElementById("canvas4");
 ctx = canvas.getContext("2d");
 
 //fill in code for canvas4 here
+
+for(let y = 0; y <= 800; y += 100){
+	for(let x = 0; x <= 800; x += 100){
+		if((x + y) % 40 === 0){
+			ctx.fillRect(x, y, 100, 100);
+		}
+	}
+}
